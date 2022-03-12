2b5454d6a63412d7bf737c985868ebf6debc5d84
Valentin <jobedan@gmail.com>
2022-03-12 22:07:16 +0400

    tabulation fixed

--- lesson2/task3.rb
+++ lesson2/task3.rb
@@ -9,10 +9,10 @@ sides = [side_a.to_f, side_b.to_f, side_c.to_f]
 max_side = sides.sort!.pop
 
 if (side_a == side_b) && (side_b == side_c)
-    puts "Triangle is perfect"
+  puts "Triangle is perfect"
 elsif (side_a == side_b) || (side_b == side_c) || (side_a == side_c)
-    puts "Triangle is equal-sided"
+  puts "Triangle is equal-sided"
 elsif  max_side ** 2 == sides.inject(0) {|sum, n| sum + n**2}
-    puts "Traingle is normal"
+  puts "Traingle is normal"
 end
 
--- lesson2/task4.rb
+++ lesson2/task4.rb
@@ -9,13 +9,13 @@ c = gets.to_f
 d = b**2 - 4*a*c
 
 if d < 0 
-    puts "no roots!"
+  puts "no roots!"
 elsif d == 0 
-    x = -b / (2 * a)
-    puts "root is #{x.ceil(2)}"
+  x = -b / (2 * a)
+  puts "root is #{x.ceil(2)}"
 else 
-    x1 = (-b + Math.sqrt(d)) / (2 * a)
-    x2 = (-b - Math.sqrt(d)) / (2 * a)
-    puts "first root is #{x1.ceil(2)}"
-    puts "second root is #{x2.ceil(2)}"
+  x1 = (-b + Math.sqrt(d)) / (2 * a)
+  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  puts "first root is #{x1.ceil(2)}"
+  puts "second root is #{x2.ceil(2)}"
 end
